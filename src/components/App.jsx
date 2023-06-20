60d210f8cb3002d365d08a03001ced1b10575fdf
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-06-20 11:52:08 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import user from '../user.json'
 import data from '../data.json'
 import friends from '../friends.json'
@@ -8,27 +8,66 @@ import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
-export const App1 = () => {
-  return <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-};
 
 
-export const App2 = () => {
-  return <Statistics title="Upload stats" stats={data} />
+  const stylePage = {
+    position: "absolute",
+    bottom: "5vh",
+    left: "50%",
+    transform: "translateX(-50%)",
+    display: "flex",
+    flexDirection: "row",
+    gap: 20
+  }
+  const styleBtn = {
+    width: 80,
+    padding: 10,
+    borderRadius: 20,
+    border: "transparent",
+    backgroundColor: "goldenrod",
+  }
 
-};
+export class App extends Component {
+ state={
+  taskNumber: 1
+  }
+  
+  nextPage = (e) => {
+    this.setState((prevState) => {
+      if (prevState.taskNumber === 4) {
+        return
+      }
+      return { taskNumber: prevState.taskNumber + 1 }
+   })
+  }
+  
+   prevPage = (e) => {
+     this.setState((prevState) => {
+       if (prevState.taskNumber === 1) {
+        return
+       }
+      return { taskNumber: prevState.taskNumber - 1 }
+   })
+ }
 
-export const App3 = () => {
-  return <FriendList friends={friends} />
-};
-
-export const App4 = () => {
-  return <TransactionHistory items={transactions} />
+  render() {
+    const {taskNumber} = this.state
+    return <>
+    {this.state.taskNumber === 1 && <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />}
+    {taskNumber === 2 && <Statistics title="Upload stats" stats={data} />}
+    {taskNumber === 3 && <FriendList friends={friends} />}
+    {taskNumber === 4 && <TransactionHistory items={transactions} />}
+ 
+      <div style={stylePage}>
+      <button style={styleBtn}  id='button-p' disabled={taskNumber===1} onClick={this.prevPage}>Prev</button>
+      <button style={styleBtn} id='button-n' disabled={taskNumber===4} onClick={this.nextPage}>Next</button>
+        </div>
+  </>
+}
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App1, App2, App3, App4 } from 'components/App';
+import { App } from 'components/App';
 import './index.css';
 
-let counter = 1;
-let App = App1;
 let root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -13,81 +11,3 @@ root.render(
   </React.StrictMode>
 )
 
-
-
-///
-
-function updateApp() {
-  switch (counter) {
-    case 1:
-      App = App1;
-      break;
-    case 2:
-      App = App2;
-      break;
-    case 3:
-      App = App3;
-      break;
-    case 4:
-      App = App4;
-      break;
-    default:
-      App = App1;
-      break;
-  }
-
-  const stylePage = {
-    position: "absolute",
-    bottom: "5vh",
-    left: "50%",
-    transform: "translateX(-50%)",
-    display: "flex",
-    flexDirection: "row",
-    gap: 20
-  }
-  const styleBtn = {
-    width: 80,
-    padding: 10,
-    borderRadius: 20,
-    border: "transparent",
-    backgroundColor: "goldenrod",
-  }
-
-  root.render(
-    <React.StrictMode>
-      <App />
-      <div style={stylePage}>
-        
-      <button style={styleBtn} disabled id='button-p'>Prev</button>
-      <button style={styleBtn} id='button-n'>Next</button>
-        </div>
-    </React.StrictMode>
-  );
-}
-
-setTimeout(() => {
-  const btnPrev = document.getElementById('button-p');
-  const btnNext = document.getElementById('button-n');
-  
-
-btnPrev.addEventListener('click', () => {
-  if (counter > 1) {
-    counter -= 1;
-  } else {
-    counter = 4
-  }
-  updateApp();
-});
-
-  btnNext.addEventListener('click', () => {
-    btnPrev.disabled = false
-  if (counter < 4) {
-    counter += 1;
-  } else {
-    counter = 1
-  }
-  updateApp();
-});
-}, 0);
-
-  updateApp()
